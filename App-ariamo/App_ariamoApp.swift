4d76a24632e2f8c39eac4956c42a3b002de451fc
Marco Antonio Donatore <marco.donatore@gmail.com>
2025-12-09 14:28:21 +0100

    Initial Commit

---
+++ App-ariamo/App_ariamoApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_ariamoApp.swift
+//  App-ariamo
+//
+//  Created by AFP FED 59 on 09/12/25.
+//
+
+import SwiftUI
+
+@main
+struct App_ariamoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
